ec542949392470256e1106eeb8154be691ac8245
Benn Stancil <bennstancil@gmail.com>
2025-09-22 18:23:57 -0400

    removing timestamps from seeds

--- tasks/analytics_engineering001/tests/columns_in_project.sql
+++ tasks/analytics_engineering001/tests/columns_in_project.sql
@@ -1,9 +1,9 @@
 with matches as (
-  select count(*) as column_count 
+  select count(*) as column_count
   from "information_schema"."columns"
   where table_catalog not in ('temp','localmemdb','_duckdb_ui')
 )
 
-select * 
-from matches 
-where column_count != 545
+select *
+from matches
+where column_count != 519
