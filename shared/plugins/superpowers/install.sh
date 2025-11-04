4df4f8abf3eded78ab1d6b9e36e58e46810492e9
CL Kao <clkao@datarecce.io>
2025-11-04 11:43:39 -0800

    Allow Skill and other tools for Claude, and install claude for superpowers before we have proper hooks for post-agent-install
    Also have git otherwise Claude get pretty confused.

--- shared/plugins/superpowers/install.sh
+++ shared/plugins/superpowers/install.sh
@@ -3,6 +3,10 @@ set -e
 
 echo "Installing superpowers for Claude Code..."
 
+
+# XXX: right now there's no post-agent-instaill hook, so we are just installing claude-code here
+npm install -g @anthropic-ai/claude-code
+
 # Add superpowers marketplace
 claude plugin marketplace add obra/superpowers-marketplace
 
