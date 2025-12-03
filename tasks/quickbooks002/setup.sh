8506a9b80880ffe4b1a2db015d978887ca176c77
Benn Stancil <bennstancil@gmail.com>
2025-12-02 19:40:54 -0500

    quickbooks fix

--- tasks/quickbooks002/setup.sh
+++ tasks/quickbooks002/setup.sh
@@ -1,27 +1,3 @@
 #!/bin/bash
 
-## Fix the data type issue.
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  SED_CMD=(sed -i '')
-else
-  SED_CMD=(sed -i)
-fi
-
-
-## Update estimate stg model
-f1="cast( {{ dbt.date_trunc('day', 'due_date') }} as date) as due_date,"
-r1="cast( {{ dbt.date_trunc('day', 'cast(due_date as date)') }} as date) as due_date,"
-
-"${SED_CMD[@]}" "s/${f1}/${r1}/g" dbt_packages/quickbooks_source/models/stg_quickbooks__estimate.sql
-
-
-## Update sales receipt and refund receipt stg models
-f2="cast( {{ dbt.date_trunc('day', 'transaction_date') }} as date) as transaction_date,"
-r2="cast( {{ dbt.date_trunc('day', 'cast(transaction_date as date)') }} as date) as transaction_date,"
-
-"${SED_CMD[@]}" "s/${f2}/${r2}/g" dbt_packages/quickbooks_source/models/stg_quickbooks__sales_receipt.sql
-"${SED_CMD[@]}" "s/${f2}/${r2}/g" dbt_packages/quickbooks_source/models/stg_quickbooks__refund_receipt.sql
-
-
-# Run dbt to create the models
 dbt run
--- tasks/quickbooks003/setup.sh
+++ tasks/quickbooks003/setup.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
 
+## Get the schema based on the database type.
+if [[ "$*" == *"--db-type=duckdb"* ]]; then
+    schema='main'
+else
+    schema='public'
+fi
+
+# Execute SQL using the run_sql utility.
+/scripts/run_sql.sh "$@" << SQL
+create or replace table ${schema}.refund_receipt_data_temp as
+  select * replace (transaction_date::integer as transaction_date)
+  from ${schema}.refund_receipt_data;
+
+create or replace table ${schema}.sales_receipt_data_temp as
+  select * replace (transaction_date::integer as transaction_date)
+  from ${schema}.sales_receipt_data;
+
+create or replace table ${schema}.estimate_data_temp as
+  select * replace (due_date::integer as due_date)
+  from ${schema}.estimate_data;
+
+drop table ${schema}.sales_receipt_data;
+drop table ${schema}.refund_receipt_data;
+drop table ${schema}.estimate_data;
+
+alter table ${schema}.sales_receipt_data_temp rename to sales_receipt_data;
+alter table ${schema}.refund_receipt_data_temp rename to refund_receipt_data;
+alter table ${schema}.estimate_data_temp rename to estimate_data;
+SQL
+
 # Run dbt to create the models
 dbt run
